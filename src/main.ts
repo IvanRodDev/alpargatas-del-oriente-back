ccc0ceec72ea849e5167566a5a387e70f5709a66
IvanRodDev <splml1014@gmail.com>
2024-09-09 11:57:29 -0500

    Add a libraries required and adittional config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { cors } from './constants';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors(cors);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.setGlobalPrefix('api');
   await app.listen(3000);
-
-    app.setGlobalPrefix('api');
-
 }
 bootstrap();
