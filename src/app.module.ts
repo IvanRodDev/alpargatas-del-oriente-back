778cacddcdfcb380aa1426fff4417b8756733511
IvanRodDev <splml1014@gmail.com>
2024-09-17 15:31:39 -0500

    Add a database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import * as Joi from 'joi';
-import { enviroments } from './config/enviroments';
-import config from './config/config';
+
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
-      load: [config],
-      isGlobal: true,
-      validationSchema: Joi.object({
-        POSTGRES_DB: Joi.string().required(),
-        POSTGRES_USER: Joi.string().required(),
-        POSTGRES_PASSWORD: Joi.string().required(),
-        POSTGRES_PORT: Joi.number().required(),
-        POSTGRES_HOST: Joi.string().required(),
-      }),
-    }),
+    DatabaseModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/modules/database/database.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
+import { enviroments } from 'src/config/enviroments';
+
+import config from 'src/config/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        POSTGRES_DB: Joi.string().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_HOST: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+})
+export class DatabaseModule {
+}
