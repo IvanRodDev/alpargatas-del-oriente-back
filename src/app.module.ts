10870e6898910b6070109d3711724803cb9c35e1
IvanRodDev <splml1014@gmail.com>
2024-09-30 16:29:10 -0500

    Add a modules in module general

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
-
+import { ProductsModule } from './modules/products/products.module';
 import { DatabaseModule } from './modules/database/database.module';
+import { UsersModule } from './modules/users/users.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { ShopCartModule } from './modules/shop-cart/shop-cart.module';
+import { OrderDetailsModule } from './modules/order-details/order-details.module';
 
 @Module({
   imports: [
+    ProductsModule,
     DatabaseModule,
+    UsersModule,
+    CategoriesModule,
+    OrdersModule,
+    ShopCartModule,
+    OrderDetailsModule,
   ],
-  controllers: [],
   providers: [],
 })
 export class AppModule {}
