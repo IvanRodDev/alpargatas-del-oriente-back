18e367a14b04ef0bbfaa28c0f90c819814a9fb9c
IvanRodDev <splml1014@gmail.com>
2024-09-30 15:39:30 -0500

    Fix a load entities

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -27,10 +27,9 @@ import config from 'src/config/config';
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       synchronize: true,
+      autoLoadEntities: true,
     }),
   ],
 })
-export class DatabaseModule {
-}
+export class DatabaseModule {}
