0526193887d68bf6eca06b3d8514aac2a8ec77db
IvanRodDev <splml1014@gmail.com>
2024-09-30 16:57:35 -0500

    Add a imports entitiy

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './controllers/orders.controller';
 import { OrdersService } from './services/orders.service';
+import { Orders } from './entities/orders.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Orders])],
   controllers: [OrdersController],
-  providers: [OrdersService]
+  providers: [OrdersService],
 })
 export class OrdersModule {}
