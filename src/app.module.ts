409db74ad2e31a7b8776ce561de295d365c90e33
IvanRodDev <splml1014@gmail.com>
2024-10-07 11:50:33 -0500

    Delete a module order-details

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { UsersModule } from './modules/users/users.module';
 import { CategoriesModule } from './modules/categories/categories.module';
 import { OrdersModule } from './modules/orders/orders.module';
 import { ShopCartModule } from './modules/shop-cart/shop-cart.module';
-import { OrderDetailsModule } from './modules/order-details/order-details.module';
 
 @Module({
   imports: [
@@ -15,7 +14,6 @@ import { OrderDetailsModule } from './modules/order-details/order-details.module
     CategoriesModule,
     OrdersModule,
     ShopCartModule,
-    OrderDetailsModule,
   ],
   providers: [],
 })
