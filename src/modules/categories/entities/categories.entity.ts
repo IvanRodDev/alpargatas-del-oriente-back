ff04a47e65f6a1d0cabbab16c70b3b359d0188d3
IvanRodDev <splml1014@gmail.com>
2024-10-10 11:34:35 -0500

    Refactorized Categories Entity

--- src/modules/categories/entities/categories.entity.ts
+++ src/modules/categories/entities/categories.entity.ts
@@ -1,4 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn, OneToMany, ManyToOne, CreateDateColumn,} from 'typeorm';
+import { Lines } from 'src/modules/lines/entities/lines.entity';
+import { Products } from 'src/modules/products/entities/products.entity';
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany, ManyToOne, CreateDateColumn, ManyToMany, JoinTable,} from 'typeorm';
 
 @Entity()
 export class Categories {
@@ -10,4 +12,19 @@ export class Categories {
 
   @Column({ type: 'varchar', length: 255 })
   description: string;
+
+  //Relacion N:N Categories-Lines
+  @ManyToMany(() => Lines, (lines) => lines.categories)
+  @JoinTable({
+    name: 'Categories_Lines',
+    joinColumn: {
+      name: 'Categorie_id',
+      referencedColumnName: 'idCategorie',
+    },
+    inverseJoinColumn: {
+      name: 'Line_id',
+      referencedColumnName: 'idLine',
+    },
+  })
+  lines: Lines[];
 }
