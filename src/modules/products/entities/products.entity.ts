f1c54ecf8e38f015ce8d8e6b9ddb64ff8863f7b9
IvanRodDev <splml1014@gmail.com>
2024-10-10 11:34:10 -0500

    Refactorized Products Entity

--- src/modules/products/entities/products.entity.ts
+++ src/modules/products/entities/products.entity.ts
@@ -1,12 +1,21 @@
-import { Column, Entity, PrimaryGeneratedColumn, OneToMany, ManyToOne, CreateDateColumn,} from 'typeorm';
+import { Lines } from 'src/modules/lines/entities/lines.entity';
+import { Orders } from 'src/modules/orders/entities/orders.entity';
+import { SubLines } from 'src/modules/sub-lines/entities/sub-lines.entity';
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne, CreateDateColumn, ManyToMany, JoinTable,} from 'typeorm';
 
 @Entity()
 export class Products {
   @PrimaryGeneratedColumn()
   idProduct: number;
 
+  @Column({ type: 'integer' })
+  code: number;
+
   @Column({ type: 'varchar', length: 255 })
   nameProduct: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  description: string;
 
   @Column({ type: 'varchar', length: 255 })
   image: string;
@@ -18,8 +27,44 @@ export class Products {
   size: number;
 
   @Column({ type: 'integer' })
+  stock: number;
+
+  @Column({ type: 'integer' })
   material: string;
 
   @CreateDateColumn({ type: 'timestamp' })
   createAt: Date;
+
+  @ManyToOne(() => Orders, (orders) => orders.products)
+  orders: Orders;
+
+  //Relacion N:N products-lines
+  @ManyToMany(() => Lines, (lines) => lines.products)
+  @JoinTable({
+    name: 'Products_Lines',
+    joinColumn: {
+      name: 'product_id',
+      referencedColumnName: 'idProduct',
+    },
+    inverseJoinColumn: {
+      name: 'line_id',
+      referencedColumnName: 'idLine',
+    },
+  })
+  lines: Lines[];
+
+  //Relacion N:N products-SubLines
+  @ManyToMany(() => SubLines, (subLines) => subLines.products)
+  @JoinTable({
+    name: 'Products_SubLines',
+    joinColumn: {
+      name: 'product_id',
+      referencedColumnName:"idProduct"
+    },
+    inverseJoinColumn: {
+      name: 'SubLine_id',
+      referencedColumnName:"idSubLine"
+    },
+  })
+  subLines: SubLines[];
 }
