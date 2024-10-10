6e7b0920f814b45b53efcc1922dbf906257f3ce2
IvanRodDev <splml1014@gmail.com>
2024-10-10 11:34:19 -0500

    Refactorized Orders Entity

--- src/modules/orders/entities/orders.entity.ts
+++ src/modules/orders/entities/orders.entity.ts
@@ -1,13 +1,22 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Products } from 'src/modules/products/entities/products.entity';
+import { ShopCart } from 'src/modules/shop-cart/entities/shop-cart.entity';
+import { Column, Entity, PrimaryGeneratedColumn,OneToOne, OneToMany } from 'typeorm';
 
 
 @Entity()
-export class Orders{
-    @PrimaryGeneratedColumn()
-    idOrder: number
+export class Orders {
+  @PrimaryGeneratedColumn()
+  idOrder: number;
 
-    @Column({ type: 'varchar', length: 255 })
-    totalAmount: number;
+  @Column({ type: 'integer' })
+  code: number;
 
+  @Column({ type: 'varchar', length: 255 })
+  totalAmount: number;
 
+  @OneToOne(() => ShopCart, (shopcart) => shopcart.orders)
+  shopCart: ShopCart;
+
+  @OneToMany(() => Products, (products) => products.orders)
+  products: Products[];
 }
