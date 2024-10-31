2f35c33476ade64bc3e35ac8a504acfa1d9152e9
IvanRodDev <splml1014@gmail.com>
2024-10-31 07:48:45 -0500

    create endpoint getAll

--- src/modules/categories/controllers/categories.controller.ts
+++ src/modules/categories/controllers/categories.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CategoriesService } from '../services/categories.service';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(
+    private categoriesService: CategoriesService,
+  ) {}
+
+  @Get('/getAll')
+  getCategories() {
+    return this.categoriesService.getCategories(); 
+  }
+}
--- src/modules/categories/services/categories.service.ts
+++ src/modules/categories/services/categories.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Categories } from 'src/modules/categories/entities/categories.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(@InjectRepository(Categories)  private cateogiresRepository: Repository<Categories>, ) {}
+
+  getCategories() {
+    return this.cateogiresRepository.find({
+      relations: ['lines','lines.subLines','lines.products','lines.subLines.products'],
+    });
+  }
+}
